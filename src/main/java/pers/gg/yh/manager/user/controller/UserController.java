bb9c28936771d1a004f84fb65ad39d294166625a
gg <401261539@qq.com>
2021-05-31 19:52:42 +0800

    clean lambo warning message.

--- src/main/java/pers/gg/yh/manager/user/controller/UserController.java
+++ src/main/java/pers/gg/yh/manager/user/controller/UserController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -39,7 +38,7 @@ public class UserController extends AbstractController {
 
             return successResponse("Login successful", token);
         } catch (Exception exception) {
-            return new Result<String>(500, exception.getMessage());
+            return new Result<String>(HttpStatus.UNAUTHORIZED.value(), exception.getMessage());
         }
 	}
 	
